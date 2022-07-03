61bfef849d62b1544c955745d819500adffeb38d
Chenyang Zhong <zhongcy95@gmail.com>
2022-07-03 06:25:16 -0400

    init: remove extra boot_init_stack_canary()
    Pretty sure this was an oversight in upstream when backporting the
    whole random stack.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Change-Id: Icffa692fca05d408abe53070e2f8b47f9d7ccaa4

--- init/main.c
+++ init/main.c
@@ -508,7 +508,6 @@ asmlinkage __visible void __init start_kernel(void)
 	 * Set up the the initial canary ASAP:
 	 */
 	add_latent_entropy();
-	boot_init_stack_canary();
 	mm_init_cpumask(&init_mm);
 	setup_command_line(command_line);
 	setup_nr_cpu_ids();
